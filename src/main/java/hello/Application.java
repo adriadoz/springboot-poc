e59dfdefa9110dddfd72bcaee660f00593e2ac17
Oriol Ylla <uriylla@gmail.com>
2018-05-08 19:21:39 +0200

    JPA integration

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -1,17 +1,23 @@
 package hello;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.boot.CommandLineRunner;
+
+
 
 @SpringBootApplication
 public class Application {
 
-    private static final Logger log = LoggerFactory.getLogger(Application.class);
-
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+    
+    @Bean
+	public CommandLineRunner demo(GreetingRepository repository) {
+		return (args) -> {
+			repository.save(new Greeting("Juanitu"));
+		};
     }
 }
